9b580409f9d1ad3319aafd5f2dedddf156fd0a04
Stepniarz <98mati06@wp.pl>
2020-06-06 13:30:31 +0200

    Enums

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,16 @@
 package com.company;
 
 import com.company.Humans.Human;
+import com.company.Vehicles.Car;
 
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-      Menu.Menu();
+     // Menu.Menu();
+        System.out.println(Car.getRandomPart());
+        System.out.println(Car.getRandomProducer());
+        System.out.println((Car.getRandomColor()));
     }
 }
--- src/com/company/Vehicles/Car.java
+++ src/com/company/Vehicles/Car.java
@@ -1,5 +1,7 @@
 package com.company.Vehicles;
 
+import java.util.Random;
+
 public class Car {
     public double value;
     public String producer;
@@ -7,6 +9,14 @@ public class Car {
     public double mileage;
     public String color;
     public String segment;
+    //Enums
+    private static final brokenParts[] BROKEN_PARTS = brokenParts.values();
+    private static final carProducer[] CAR_PRODUCER = carProducer.values();
+    private static final carColor[] CAR_COLOR = carColor.values();
+    private static final int PARTS_AMOUNT = BROKEN_PARTS.length;
+    private static final int PRODUCERS_AMOUNT = CAR_PRODUCER.length;
+    private static final int CAR_COLORS = CAR_COLOR.length;
+    private static final Random RANDOM = new Random();
 
 
 
@@ -18,4 +28,45 @@ public class Car {
         this.color = color;
         this.segment = segment;
     }
+    //Randomly picked broken parts of the vehicle
+    public enum brokenParts{
+        BREAKS,
+        SUSPENSION,
+        ENGINE,
+        BODY,
+        TRANSMISSION
+    }
+    public enum carProducer{
+        VOLVO,
+        OPEL,
+        FIAT,
+        TOYOTA,
+        RENO
+    }
+    public enum carColor{
+        WHITE,
+        BLACK,
+        BLUE,
+        YELLOW,
+        GREEN,
+        SILVER,
+        BROWN,
+        RED,
+        PINK,
+        PURPLE,
+
+    }
+    public static brokenParts getRandomPart(){
+        return BROKEN_PARTS[RANDOM.nextInt(PARTS_AMOUNT)];
+    }
+    public static carProducer getRandomProducer(){
+        return  CAR_PRODUCER[RANDOM.nextInt((PRODUCERS_AMOUNT))];
+    }
+    public static carColor getRandomColor(){
+        return CAR_COLOR[RANDOM.nextInt((CAR_COLORS))];
+    }
 }
+
+    //
+
+
