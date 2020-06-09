8511a82506a7b52fc5173753e2b08ff753042151
Stepniarz <98mati06@wp.pl>
2020-06-09 20:41:50 +0200

    Enum to list, RNG

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,14 +3,22 @@ package com.company;
 import com.company.Humans.Human;
 import com.company.Vehicles.Car;
 
+import java.awt.*;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
      // Menu.Menu();
-        System.out.println(Car.getRandomPart());
-        System.out.println(Car.getRandomProducer());
-        System.out.println((Car.getRandomColor()));
+//        System.out.println(Car.getRandomPart());
+//        System.out.println(Car.getRandomProducer());
+//        System.out.println((Car.getRandomColor()));
+//        Car car = new Car("Volvo","Peugot",5600.0,115000.0,"Purple","Tak");
+//        System.out.println(car.brokenPartsList);
+
+        for (int i = 0; i < 5; i = i + 2){
+            System.out.println(i);
+        }
+
     }
 }
--- src/com/company/Vehicles/Car.java
+++ src/com/company/Vehicles/Car.java
@@ -1,22 +1,28 @@
 package com.company.Vehicles;
 
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 
-public class Car {
+public class Car{
     public double value;
     public String producer;
     public String model;
     public double mileage;
     public String color;
     public String segment;
+    public List<brokenPart> brokenPartsList = new ArrayList<>();
     //Enums
-    private static final brokenParts[] BROKEN_PARTS = brokenParts.values();
+    private static final brokenPart[] BROKEN_PARTS = brokenPart.values();
     private static final carProducer[] CAR_PRODUCER = carProducer.values();
     private static final carColor[] CAR_COLOR = carColor.values();
     private static final int PARTS_AMOUNT = BROKEN_PARTS.length;
     private static final int PRODUCERS_AMOUNT = CAR_PRODUCER.length;
-    private static final int CAR_COLORS = CAR_COLOR.length;
-    private static final Random RANDOM = new Random();
+    private static final int COLORS_AMOUNT = CAR_COLOR.length;
+
+
 
 
 
@@ -27,14 +33,24 @@ public class Car {
         this.mileage = mileage;
         this.color = color;
         this.segment = segment;
+        this.brokenPartsList = getRandomParts();
+
     }
-    //Randomly picked broken parts of the vehicle
-    public enum brokenParts{
-        BREAKS,
-        SUSPENSION,
-        ENGINE,
-        BODY,
-        TRANSMISSION
+    //Broken parts of the vehicle
+    public enum brokenPart{
+        BREAKS(1),
+        SUSPENSION(2),
+        ENGINE(3),
+        BODY(4),
+        TRANSMISSION(5);
+
+        int value;
+        brokenPart(int i) {
+            this.value = i;
+        }
+        public int getBrokenPartIndex(){
+            return this.value;
+        }
     }
     public enum carProducer{
         VOLVO,
@@ -54,19 +70,38 @@ public class Car {
         RED,
         PINK,
         PURPLE,
+    }
 
-    }
-    public static brokenParts getRandomPart(){
-        return BROKEN_PARTS[RANDOM.nextInt(PARTS_AMOUNT)];
-    }
-    public static carProducer getRandomProducer(){
-        return  CAR_PRODUCER[RANDOM.nextInt((PRODUCERS_AMOUNT))];
-    }
-    public static carColor getRandomColor(){
-        return CAR_COLOR[RANDOM.nextInt((CAR_COLORS))];
-    }
-}
+    private List<brokenPart> getRandomParts() {
+            List<brokenPart> listToReturn = new ArrayList<>();
+            int numberOfBrokenParts = numberCheck(0.35);
+            Random rand = new Random();
+            for (int i = 0;i < numberOfBrokenParts ;i++){
+                brokenPart partToAdd = brokenPart.values()[rand.nextInt(5)];
+                if(!listToReturn.contains(partToAdd))
+                    listToReturn.add(partToAdd);
+            }
+            return listToReturn;
+        }
+//    private carProducer getRandomProducer () {
+//        return CAR_PRODUCER[RANDOM.nextInt((PRODUCERS_AMOUNT))];
+//    }
+//    private carColor getRandomColor () {
+//        return CAR_COLOR[RANDOM.nextInt((COLORS_AMOUNT))];
+//    }
 
-    //
+    private int numberCheck(double possibilty) {
+        int numberOfParts = -1;
+        double chance;
+        do{
+            chance = Math.random();
+            numberOfParts++;
+        }
+        while(chance < possibilty);
+        return numberOfParts;
+    }
+    }
+
+
 
 
